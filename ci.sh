2d12f75f2849affbb9eef94bc045b33e288e6212
Pam Rucinque <pamela.rucinque@gmail.com>
2017-01-15 12:37:15 +1100

    Ugly URLS works throught the cli

--- ci.sh
+++ ci.sh
@@ -7,6 +7,6 @@ S3_BUCKET=hackforprivacy.org
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${DOWNLOAD_FILE_NAME}
 tar xvzf ${DOWNLOAD_FILE_NAME}
 
-./hugo -v
+./hugo -v --uglyURLs
 
 aws s3 sync public/ s3://${S3_BUCKET} --acl public-read --region ap-southeast-2 --delete --cache-control "max-age=300,s-maxage=300"
